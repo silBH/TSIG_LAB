f6fdcc7fbd54435bcf6686d3ad64dc04461d0173
Santiago Gamarra <santi.gamarra@gmail.com>
2023-05-27 18:54:59 -0300

    crear admin implementado

--- TSIG_LAB-ejb/src/main/java/dao/AdministradorDAO.java
+++ TSIG_LAB-ejb/src/main/java/dao/AdministradorDAO.java
@@ -1,47 +1,57 @@
 package dao;
 
 import java.util.List;
-import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import entity.Administrador;
-import persistence.Conexion;
-
-//estas funciones solo se usan desde la interfaz local
 
 @Stateless
-@LocalBean
 public class AdministradorDAO implements AdministradorDAOLocal {
 
-	Conexion conexion = Conexion.getInstancia();
-	private EntityManager em = conexion.getEntityManager(); 
-	
+    @PersistenceContext(unitName = "TSIG_LABPersistenceUnit")
+    private EntityManager em;
+
     public AdministradorDAO() {
     }
 
     @Override
-	public List<Administrador> listar(){
-    	Query q = em.createNativeQuery("select * from Administrador", Administrador.class);
-		List<Administrador> result = q.getResultList();
-    	return result;
-	}
-    
-    @Override
-	public Administrador obtenerPorId(Long id) {
-    	//completar
-		return null;
-	}
-    
-    @Override
-	public Administrador obtenerPorUsername(String username) {
-    	//completar
-    	return null;
+    public List<Administrador> listar() {
+        Query q = em.createQuery("SELECT a FROM Administrador a", Administrador.class);
+        List<Administrador> result = q.getResultList();
+        return result;
     }
-	
-	//crear si son necesarias porque no se pide modificaciones del administrador
-	//public void crear(Administrador admin);
-	//public void editar(Administrador admin);
-	//public void eliminar(Administrador admin);
+
+    @Override
+    public Administrador obtenerPorId(Long id) {
+        return em.find(Administrador.class, id);
+    }
+
+    @Override
+    public Administrador obtenerPorUsername(String username) {
+        Query query = em.createQuery("SELECT a FROM Administrador a WHERE a.username = :username", Administrador.class);
+        query.setParameter("username", username);
+        List<Administrador> result = query.getResultList();
+        if (!result.isEmpty()) {
+            return result.get(0);
+        }
+        return null;
+    }
+
+    @Override
+    public void crear(Administrador admin) {
+        em.persist(admin);
+    }
+
+    @Override
+    public void editar(Administrador admin) {
+        em.merge(admin);
+    }
+
+    @Override
+    public void eliminar(Administrador admin) {
+        em.remove(em.merge(admin));
+    }
 }
